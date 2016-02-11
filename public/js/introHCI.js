af447445fcbf538a40cf34889ecd56f25bef4535
Weichen  Liu <wel174@ucsd.edu>
2016-02-11 18:44:01 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,7 +27,22 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var getURL = "/project/"+idNumber;
+
+	console.log("GET URL: " + getURL);
+
+	$.get(getURL, getProjectDetails);
 }
+
+function getProjectDetails(result){
+	console.log(result);
+
+	var detailsHTML = "<div class='details'>"+result["summary"]+"<img class='detailsImage' src="+result["image"]+"></img></div>"
+
+	$("#project"+result["id"]).find(".details").html(detailsHTML);
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +50,21 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", changeColor);
+
+}
+
+function changeColor(result){
+
+	var colors = result["colors"]["hex"];
+
+	console.log(colors);
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
